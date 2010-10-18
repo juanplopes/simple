cbc313e61ddb32129d8f56ac08a4e2dc5fdafed2
Juan Lopes <juanplopes@gmail.com>
2010-10-18 03:20:53 -0200

    New way of generating third part of version number

---
+++ src-tools/Simple.Tools.MsBuild/DaysSince.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+
+namespace Simple.Tools.MsBuild
+{
+    public class DaysSince : Task
+    {
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Output]
+        public ITaskItem Output { get; set; }
+
+        public override bool Execute()
+        {
+            Output = new TaskItem(((int)DateTime.Now.Subtract(StartDate).TotalDays).ToString());
+
+            return true;
+        }
+    }
+}
