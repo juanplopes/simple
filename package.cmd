cac9258b90b986012089849a58253bbedeebe89c
Juan Lopes <juanplopes@gmail.com>
2010-10-19 00:38:37 -0200

    Removing publish support for Google Code

--- package.cmd
+++
@@ -1,1 +0,0 @@
-call build Package
--- publish.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-setlocal
-set /p GC_USER="Username: "
-set /p GC_PASS="Password: "
-call build Publish "/property:GCUsername=%GC_USER%" "/property:GCPassword=%GC_PASS%"
-endlocal
