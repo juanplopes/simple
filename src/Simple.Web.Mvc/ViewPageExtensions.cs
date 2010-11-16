638434f0a96181fd53eb35ef67cef6c8d99bedbb
Juan Lopes <juanplopes@gmail.com>
2010-11-16 15:42:59 -0200

    Adding non-typed overload to routeparam

--- src/Simple.Web.Mvc/ViewPageExtensions.cs
+++ src/Simple.Web.Mvc/ViewPageExtensions.cs
@@ -8,21 +8,35 @@ namespace Simple.Web.Mvc
 {
     public static class ViewPageExtensions
     {
+        public static object RouteParam(this ViewUserControl page, string name)
+        {
+            return page.RouteParam<object>(name);
+        }
+
         public static T RouteParam<T>(this ViewUserControl page, string name)
         {
             object obj = page.ViewContext.RouteData.Values[name];
 
-            if (obj != null)
+            if (obj is T)
+                return (T)obj;
+            else if (obj != null)
                 return (T)Convert.ChangeType(obj, typeof(T));
             else
                 return default(T);
+        }
+
+        public static object RouteParam(this ViewPage page, string name)
+        {
+            return page.RouteParam<object>(name);
         }
 
         public static T RouteParam<T>(this ViewPage page, string name)
         {
             object obj = page.ViewContext.RouteData.Values[name];
 
-            if (obj != null)
+            if (obj is T)
+                return (T)obj;
+            else if (obj != null)
                 return (T)Convert.ChangeType(obj, typeof(T));
             else
                 return default(T);
