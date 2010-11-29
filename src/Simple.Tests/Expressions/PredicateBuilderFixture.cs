0bc150308a8123a192df5ff9b72ad160210c054a
Juan Lopes <juanplopes@gmail.com>
2010-11-29 11:37:38 -0200

    Adding tests to PredicateBuilder

---
+++ src/Simple.Tests/Expressions/PredicateBuilderFixture.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SharpTestsEx;
+using System.Linq.Expressions;
+
+namespace Simple.Tests.Expressions
+{
+    [TestFixture]
+    public class PredicateBuilderFixture
+    {
+        [Test]
+        public void CanApplyAndToConstantExpression()
+        {
+            var expr = PredicateBuilder.True<string>();
+            expr = expr.And(x => x.Length == 2);
+            expr.ToString().Should().Be("f => (True && (f.Length = 2))");
+        }
+
+        [Test]
+        public void CanApplyOrToConstantExpression()
+        {
+            var expr = PredicateBuilder.False<string>();
+            expr = expr.Or(x => x.Length == 2);
+            expr.ToString().Should().Be("f => (False || (f.Length = 2))");
+        }
+
+        [Test]
+        public void CanApplyOrToNull_ItConvertsNullToFalse()
+        {
+            var expr = PredicateBuilder.Or<string>(null, x => x.Length == 2);
+            expr.ToString().Should().Be("f => (False || (f.Length = 2))");
+        }
+
+        [Test]
+        public void CanApplyAndToNull_ItConvertsNullToTrue()
+        {
+            var expr = PredicateBuilder.And<string>(null, x => x.Length == 2);
+            expr.ToString().Should().Be("f => (True && (f.Length = 2))");
+        }
+
+        [Test]
+        public void CanApplyOrToConstantExpressionWithCustomParameter()
+        {
+            var expr = PredicateBuilder.True<string>(Expression.Parameter(typeof(string), "p"));
+            expr = expr.And(x => x.Length == 2);
+            expr.ToString().Should().Be("p => (True && (p.Length = 2))");
+        }
+
+        [Test]
+        public void CanApplyAndToConstantExpressionWithCustomParameter()
+        {
+            var expr = PredicateBuilder.False<string>(Expression.Parameter(typeof(string), "p"));
+            expr = expr.Or(x => x.Length == 2);
+            expr.ToString().Should().Be("p => (False || (p.Length = 2))");
+        }
+    }
+}
--- src/Simple/Expressions/PredicateBuilder.cs
+++ src/Simple/Expressions/PredicateBuilder.cs
@@ -7,12 +7,28 @@ namespace Simple
 {
     public static class PredicateBuilder
     {
-        public static Expression<Func<T, bool>> True<T>() { return f => true; }
-        public static Expression<Func<T, bool>> False<T>() { return f => false; }
+        public static Expression<Func<T, bool>> True<T>()
+        {
+            return True<T>(Expression.Parameter(typeof(T), "f"));
+        }
+        public static Expression<Func<T, bool>> True<T>(ParameterExpression param)
+        {
+            return Expression.Lambda<Func<T, bool>>(Expression.Constant(true), param);
+        }
+
+        public static Expression<Func<T, bool>> False<T>()
+        {
+            return False<T>(Expression.Parameter(typeof(T), "f"));
+        }
+        public static Expression<Func<T, bool>> False<T>(ParameterExpression param)
+        {
+            return Expression.Lambda<Func<T, bool>>(Expression.Constant(false), param);
+        }
 
         public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1,
                                                             Expression<Func<T, bool>> expr2)
         {
+            if (expr1 == null) expr1 = False<T>();
             var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
             return Expression.Lambda<Func<T, bool>>
                   (Expression.OrElse(expr1.Body, invokedExpr), expr1.Parameters).ExpandInvocations();
@@ -21,6 +37,8 @@ namespace Simple
         public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1,
                                                              Expression<Func<T, bool>> expr2)
         {
+            if (expr1 == null) expr1 = True<T>();
+
             var invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
             return Expression.Lambda<Func<T, bool>>
                   (Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters).ExpandInvocations();
